4c3db803f8364433e8021635df1d15946535be51
Manuel Gonzalez <mgonzalez4648@gmail.com>
2023-02-20 10:04:31 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,9 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  
+  color: white;
+  background-color: #2c3e50;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+try {
+  const { ipcRenderer } = require('electron');
+  ipcRenderer.on('SET_SCREEN_SOURCE', async (_event, sourceId) => {
+    console.log(`Setting screen source to ${sourceId}`);
+  });
+} catch (error) {
+  console.warn('Could not set screen source. Running without electron?');
+}
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
