45966052e9b20cd58aedbc19871f2e2a2282ad96
Manuel Gonzalez <mgonzalez4648@gmail.com>
2023-02-20 10:10:00 +0100

    Fix webpack < 5 issue

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  lintOnSave: false,
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      // chainWebpackMainProcess: config => {
+      //   config.module
+      //     .rule('babel')
+      //     .test(/background\.js$/)
+      //     .use('babel')
+      //     .loader('babel-loader')
+      //     .options({
+      //       presets: [['@babel/preset-env', { modules: false }]],
+      //       plugins: ['@babel/plugin-proposal-class-properties']
+      //     })
+      // }
+    },
+  },
+  configureWebpack: {
+    devtool: 'source-map',
+  },
+};
